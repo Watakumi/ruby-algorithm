86d4d712b69719c47fe00ef47d03a23fd31f497e
石渡拓海 <t-ishiwata@sight-visit.com>
2021-06-06 23:25:51 +0900

    [Change] 項目に合わせて出力する

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,6 @@
 require 'mechanize'
+
+CAL = 'cal'
 agent = Mechanize.new
 
 # ローソンのおにぎりの情報を取得
@@ -6,5 +8,17 @@ page = agent.get('https://www.lawson.co.jp/recommend/original/rice/')
 products = page.search('article section ul li')
 
 products.each do |product|
-  puts "#{product.search('p')[1].inner_text } #{product.search('p')[2].inner_text} #{product.search('p')[3].inner_text} #{product.search('p')[4]&.inner_text}"
+  elements = product.search('p')
+  title = product.search('p.ttl').inner_text.gsub("　", "")
+  price = product.search('p.price').inner_text
+
+  calorie_element_num = 0
+
+  elements.each.with_index do |element,  index|
+    calorie_element_num = index if element.inner_text.include?(CAL) 
+  end
+
+  calorie = elements[calorie_element_num].inner_text
+
+  puts "#{ title } #{ calorie } #{ price }"
 end
